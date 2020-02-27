01baaab9e8b43ef453191009f9b297a15166479c
Alejandro Vázques O <allvaso2@gmail.com>
2020-02-26 23:24:43 -0600

    Se inicia la creación del grafo

--- NodeGraph.hpp
+++ NodeGraph.hpp
@@ -1,10 +1,45 @@
 #include <iostream> 
 using namespace std;
 
-template <typename T> class NodeGraph{
+template <typename T> class nodeGraph{
     public: 
         T valor;
-        NodeGraph *anterior = nullptr;
-        NodeGraph *siguiente = nullptr;
 
 };
+
+
+class Graph{
+    
+    
+public:
+
+    int numeroDeNodos;
+    int numeroDeAristas;
+    linkedList<nodeGraph> nodos;
+    linkedList<Edge> edges;
+
+    Graph(graphEdge edges[], int n, int N)  {
+    
+        nodos = new linkedList<nodeGraph>;
+        edges = new linkedList<Edge>;
+        
+    }
+
+    int añadirNodo(int valor, int weight, adjNode* head)   {
+        nodeGraph<T> *nuevoNodo = new nodeGraph<int>;
+        nuevoNodo->valor = valor;
+        nuevoNodo->cost = weight;
+        nodos.insertarFinal(nuevoNodo);
+         numeroDeNodos++;
+        return 0;
+    }
+
+    int conectarNodo(int peso, int id, NodeGraph*<T> inicio, NodeGraph*<T> fin){
+        
+        Edge* edge = new Edge(peso,id,inicio,fin);
+        edges.insertarFinal(edge);
+        numeroDeAristas++;
+        return 0;
+    }
+    
+}
